85c626551e93b40247fcc41de25caf78b0acb045
Jonash55 <bjankowski333@gmail.com>
2021-01-12 16:47:40 +0100

    UPDATE repository space
    Usunięto nie potrzebne pliki z repozytorium

--- Bartosz Jankowski/Python część 2/InnyPlik.py
+++
@@ -1,9 +0,0 @@
-from main import FileManager
-
-print("===================================================================")
-print("                            Zadanie 9                             \n")
-
-plik = FileManager('tekst2.txt')
-plik.read_file()
-plik.update_file(' Działa!')
-plik.read_file()
--- Bartosz Jankowski/ćwiczenia1BartoszJankowkiPyFormat.py
+++
@@ -1,9 +0,0 @@
-print("{:*<18}".format("Jankowski"))
-
-print("{:>18}".format("Jankowski"))
-
-print("{:{odstep}} = {:{odstep}}".format("Jankowski", 2.21123123, odstep='.5'))
-
-print("{:{pozycja}{szerokosc}}".format("Jankowski", pozycja="^", szerokosc=100))
-
-print("{:{pozycja}{szerokosc}}".format("Jankowski", pozycja=">", szerokosc=100))
